f22bd80244f36d6f280a9094625c352a3d6f92ad
niel905g <nielsbd@outlook.com>
2020-04-17 10:49:13 +0200

    init Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+// eslint-disable-next-line
+import React, { Component } from 'react';
+// eslint-disable-next-line
+import GitHub from './GitHub';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GitHub />
     </div>
   );
 }
---
+++ src/GitHub.js
@@ -0,0 +1,87 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { Component } from 'react';
+import axios from 'axios';
+import ReactLoading from 'react-loading';
+import { Media, Form, FormGroup, FormControl, Button } from 'react-bootstrap';
+
+class GitHub extends Component {    
+    
+    constructor(){
+        super();  
+        this.state = {
+            data: [],
+            searchTerm:'',
+            isLoading : false            
+        };     
+        this.handleChange = this.handleChange.bind(this);  
+        this.handleSubmit = this.handleSubmit.bind(this);                               
+    }
+
+    handleChange(e) {
+        this.setState({ searchTerm: e.target.value });        
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();    
+        this.setState({
+            isLoading : true          
+        })                
+        this.getGitHubData(this.state.searchTerm);        
+    }      
+
+    getGitHubData(_searchTerm){                
+        axios.get("https://api.github.com/search/users?q="+_searchTerm)
+            .then(res => {                                   
+                this.setState({
+                    isLoading : false,  
+                    data: res.data.items                 
+                })                            
+                console.log(res.data.items); 
+            });             
+    }          
+
+    render() {  
+        const listUsers = this.state.data.map((user) =>             
+            <Media key={user.id}>
+              <img 
+                width={64} 
+                height={64} 
+                className="mr-3"
+                src={ user.avatar_url } 
+                alt="Image"
+              />
+
+              <Media.Body>
+                <h5>{user.login}</h5>
+                <p>Score: { user.score }</p>
+              </Media.Body>
+            </Media>          
+        );        
+        
+        return (
+          <div> 
+            <br />  
+            <Form inline onSubmit={this.handleSubmit}>
+                <FormGroup controlId="formInlineName">                                    
+                    <FormControl 
+                        type="text" 
+                        value={this.state.searchTerm} 
+                        placeholder="Enter Search Term" 
+                        onChange={this.handleChange}
+                    />
+                </FormGroup>                            
+                {' '}
+                <Button type="submit">
+                    Search
+                </Button>
+            </Form>                 
+            <h3>GitHub Users Results</h3>
+            { this.state.isLoading &&                                             
+                <ReactLoading type="spinningBubbles" color="#444" /> 
+            }                               
+            {listUsers}                                                                                            
+          </div>
+        );
+    }       
+}
+export default GitHub;
